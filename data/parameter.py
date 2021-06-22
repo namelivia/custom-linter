c88ec47995153ee0f72732d2295ba8b105189784
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-06-22 17:43:02 +0200

    Some fixes

--- data/parameter.py
+++ data/parameter.py
@@ -9,10 +9,14 @@ class Parameter:
         self.name = self.node.name.value
         self.position_index = self.node.position_index
         self.star_count = self.node.star_count
-        self.annotation = self.node.annotation.value if self.node.annotation is not None else None
+        try:
+            self.annotation = self.node.annotation.value if self.node.annotation is not None else None
+        except Exception:
+            self.annotation = self.node.annotation.get_code()
         self.default = self.node.default.value if self.node.default is not None else None
 
     def is_boolean(self):
-        if self.node.annotation is not None:
+        try:
             return self.node.annotation.value == "bool"
-        return False
+        except Exception:
+            return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from analyze import parse_file, analyze_function
 from render.render import write_render_output, render_function, render_file
-from parso.python.tree import Function
+from parso.python.tree import Function, Class, PythonNode
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -9,8 +9,14 @@ parser.add_argument("--html", type=str)
 arguments = parser.parse_args()
 
 for input_file in arguments.files:
+    result = []
     render_file(input_file)
     code = parse_file(input_file)
-    result = [analyze_function(node) for node in code.children if isinstance(node, Function)]
+    for node in code.children:
+        if isinstance(node, Class):
+            for node in node.iter_funcdefs():
+                result.append(analyze_function(node))
+        if isinstance(node, Function):
+            result.append(analyze_function(node))
     [render_function(function) for function in result]
 write_render_output(arguments.html)
